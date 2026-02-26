438b687fbbbce31b4532acb7beee2ea570131b83
bibito- <w3405034@gmail.com>
2026-02-27 01:41:15 +0900

    baseを一度書き換え。

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: "/scan-code-web/",
+   base: "/scan-code-web/",
     plugins: [
         react(),
         tailwindcss()
